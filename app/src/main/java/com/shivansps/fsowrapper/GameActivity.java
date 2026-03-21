4713d1c026ad3a51f7bdeb83f444595ba679c204
Salvador Cipolla <shivanuo@hotmail.com>
2026-03-21 15:49:59 -0300

    support fso borderless

--- app/src/main/java/com/shivansps/fsowrapper/GameActivity.java
+++ app/src/main/java/com/shivansps/fsowrapper/GameActivity.java
@@ -50,6 +50,7 @@ public class GameActivity extends org.libsdl.app.SDLActivity {
         try {
             getWindow().setSustainedPerformanceMode(true);
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            hideSystemUI();
         } catch (Throwable ignored) {
         }
         TTSManager.init(this);
@@ -67,8 +68,28 @@ public class GameActivity extends org.libsdl.app.SDLActivity {
         super.onPause();
     }
 
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
     @Override protected void onResume() {
         super.onResume();
+        hideSystemUI();
     }
 
     @Override protected void onDestroy()

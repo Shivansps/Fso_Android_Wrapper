4c080c9beb4d4098d8d89d5744959564e3a781a5
Salvador Cipolla <shivanuo@hotmail.com>
2026-05-30 23:34:48 -0300

    match setupOverlayFromXml from knossosnet

--- app/src/main/java/com/shivansps/fsowrapper/GameActivity.java
+++ app/src/main/java/com/shivansps/fsowrapper/GameActivity.java
@@ -139,139 +139,158 @@ public class GameActivity extends org.libsdl.app.SDLActivity {
     }
 
     @SuppressLint("ClickableViewAccessibility")
+    private View.OnTouchListener makeTouchHandler(int code) {
+        return (v, e) -> {
+            switch (e.getActionMasked()) {
+                case MotionEvent.ACTION_DOWN:
+                    v.setPressed(true);
+                    NativeBridge.onButton(code, true);
+                    return true;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    v.setPressed(false);
+                    NativeBridge.onButton(code, false);
+                    return true;
+            }
+            return false;
+        };
+    }
+
+    @SuppressLint({"ClickableViewAccessibility", "DiscouragedApi"})
     private void setupOverlayFromXml()
     {
-        LayoutInflater inflater = LayoutInflater.from(this);
-        View overlay = inflater.inflate(R.layout.overlay_controls, null);
-        Button btnToggle = overlay.findViewById(R.id.btnToggle);
-        RadialDpadView dpad = overlay.findViewById(R.id.dpad);
+        int layoutId = getResources().getIdentifier("overlay_controls", "layout", getPackageName());
+        View overlay = getLayoutInflater().inflate(layoutId, null);
+
+        Button btnToggle = overlay.findViewById(getResources().getIdentifier("btnToggle", "id", getPackageName()));
+        RadialDpadView dpad = overlay.findViewById(getResources().getIdentifier("dpad", "id", getPackageName()));
+
 
         // Button listeners
-        Button btnKyb = overlay.findViewById(R.id.btnKyb);
+        Button btnKyb = overlay.findViewById(getResources().getIdentifier("btnKyb", "id", getPackageName()));
         btnKyb.setOnClickListener(v -> toggleSdlKeyboard(overlay));
-
         // ESC
-        Button btnEsc = overlay.findViewById(R.id.btnEsc);
+        Button btnEsc = overlay.findViewById(getResources().getIdentifier("btnEsc", "id", getPackageName()));
         btnEsc.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_ESC));
 
         // F3
-        Button btnF3 = overlay.findViewById(R.id.btnF3);
+        Button btnF3 = overlay.findViewById(getResources().getIdentifier("btnF3", "id", getPackageName()));
         btnF3.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_F3));
 
         // ALT+J
-        Button btnALTJ = overlay.findViewById(R.id.btnAltJ);
+        Button btnALTJ = overlay.findViewById(getResources().getIdentifier("btnAltJ", "id", getPackageName()));
         btnALTJ.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_ALT_J));
 
         // ALT+M
-        Button btnALTM = overlay.findViewById(R.id.btnAltM);
+        Button btnALTM = overlay.findViewById(getResources().getIdentifier("btnAltM", "id", getPackageName()));
         btnALTM.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_ALT_M));
 
         // ALT+H
-        Button btnALTH = overlay.findViewById(R.id.btnAltH);
+        Button btnALTH = overlay.findViewById(getResources().getIdentifier("btnAltH", "id", getPackageName()));
         btnALTH.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_ALT_H));
 
         // ALT+A
-        Button btnAltA = overlay.findViewById(R.id.btnAltA);
+        Button btnAltA = overlay.findViewById(getResources().getIdentifier("btnAltA", "id", getPackageName()));
         btnAltA.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_ALT_A));
 
         // Space
-        Button btnSpace = overlay.findViewById(R.id.btnFireS);
+        Button btnSpace = overlay.findViewById(getResources().getIdentifier("btnFireS", "id", getPackageName()));
         btnSpace.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_SPACE));
 
         // LCtrl
-        Button btnLCtrl = overlay.findViewById(R.id.btnFireP);
+        Button btnLCtrl = overlay.findViewById(getResources().getIdentifier("btnFireP", "id", getPackageName()));
         btnLCtrl.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_CTRL));
 
         // CycleP
-        Button btnCycleP = overlay.findViewById(R.id.btnCycleP);
+        Button btnCycleP = overlay.findViewById(getResources().getIdentifier("btnCycleP", "id", getPackageName()));
         btnCycleP.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_CYCLE_P));
 
         // CycleS
-        Button btnCycleS = overlay.findViewById(R.id.btnCycleS);
+        Button btnCycleS = overlay.findViewById(getResources().getIdentifier("btnCycleS", "id", getPackageName()));
         btnCycleS.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_CYCLE_S));
 
         // Tab
-        Button btnTab = overlay.findViewById(R.id.btnTab);
+        Button btnTab = overlay.findViewById(getResources().getIdentifier("btnTab", "id", getPackageName()));
         btnTab.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_TAB));
 
         // +
-        Button btnPlus = overlay.findViewById(R.id.btnPlus);
+        Button btnPlus = overlay.findViewById(getResources().getIdentifier("btnPlus", "id", getPackageName()));
         btnPlus.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_PLUS));
 
         // -
-        Button btnMinus = overlay.findViewById(R.id.btnMinus);
+        Button btnMinus = overlay.findViewById(getResources().getIdentifier("btnMinus", "id", getPackageName()));
         btnMinus.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_MINUS));
 
         // Q
-        Button btnQ = overlay.findViewById(R.id.btnQ);
+        Button btnQ = overlay.findViewById(getResources().getIdentifier("btnQ", "id", getPackageName()));
         btnQ.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_Q));
 
         // -
-        Button btnX = overlay.findViewById(R.id.btnX);
+        Button btnX = overlay.findViewById(getResources().getIdentifier("btnX", "id", getPackageName()));
         btnX.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_X));
 
         // Y
-        Button btnY = overlay.findViewById(R.id.btnY);
+        Button btnY = overlay.findViewById(getResources().getIdentifier("btnY", "id", getPackageName()));
         btnY.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_Y));
 
         // H
-        Button btnH = overlay.findViewById(R.id.btnH);
+        Button btnH = overlay.findViewById(getResources().getIdentifier("btnH", "id", getPackageName()));
         btnH.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_H));
 
         // B
-        Button btnB = overlay.findViewById(R.id.btnB);
+        Button btnB = overlay.findViewById(getResources().getIdentifier("btnB", "id", getPackageName()));
         btnB.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_B));
 
         // E
-        Button btnE = overlay.findViewById(R.id.btnE);
+        Button btnE = overlay.findViewById(getResources().getIdentifier("btnE", "id", getPackageName()));
         btnE.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_E));
 
         // F
-        Button btnF = overlay.findViewById(R.id.btnF);
+        Button btnF = overlay.findViewById(getResources().getIdentifier("btnF", "id", getPackageName()));
         btnF.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_F));
 
         // T
-        Button btnT = overlay.findViewById(R.id.btnT);
+        Button btnT = overlay.findViewById(getResources().getIdentifier("btnT", "id", getPackageName()));
         btnT.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_T));
 
         // M
-        Button btnM = overlay.findViewById(R.id.btnM);
+        Button btnM = overlay.findViewById(getResources().getIdentifier("btnM", "id", getPackageName()));
         btnM.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_M));
 
         // S
-        Button btnS = overlay.findViewById(R.id.btnS);
+        Button btnS = overlay.findViewById(getResources().getIdentifier("btnS", "id", getPackageName()));
         btnS.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_S));
 
         // A
-        Button btnA = overlay.findViewById(R.id.btnA);
+        Button btnA = overlay.findViewById(getResources().getIdentifier("btnA", "id", getPackageName()));
         btnA.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_A));
 
         // Z
-        Button btnZ = overlay.findViewById(R.id.btnZ);
+        Button btnZ = overlay.findViewById(getResources().getIdentifier("btnZ", "id", getPackageName()));
         btnZ.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_Z));
 
         // Return
-        Button btnRet = overlay.findViewById(R.id.btnRet);
+        Button btnRet = overlay.findViewById(getResources().getIdentifier("btnRet", "id", getPackageName()));
         btnRet.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_BACKSPACE));
 
         // Backslash
-        Button btnBackSlash = overlay.findViewById(R.id.btnBackSlash);
+        Button btnBackSlash = overlay.findViewById(getResources().getIdentifier("btnBackSlash", "id", getPackageName()));
         btnBackSlash.setOnTouchListener(makeTouchHandler(NativeBridge.CODE_KEY_BACKSLASH));
 
         // C+3+1
-        Button btnC31 = overlay.findViewById(R.id.btnC31);
+        Button btnC31 = overlay.findViewById(getResources().getIdentifier("btnC31", "id", getPackageName()));
         btnC31.setOnClickListener(v -> NativeBridge.runMacro(NativeBridge.C_3_1));
 
         // C+3+5
-        Button btnC35 = overlay.findViewById(R.id.btnC35);
+        Button btnC35 = overlay.findViewById(getResources().getIdentifier("btnC35", "id", getPackageName()));
         btnC35.setOnClickListener(v -> NativeBridge.runMacro(NativeBridge.C_3_5));
 
         // C+3+9
-        Button btnC39 = overlay.findViewById(R.id.btnC39);
+        Button btnC39 = overlay.findViewById(getResources().getIdentifier("btnC39", "id", getPackageName()));
         btnC39.setOnClickListener(v -> NativeBridge.runMacro(NativeBridge.C_3_9));
 
         // C+5
-        Button btnC5 = overlay.findViewById(R.id.btnC5);
+        Button btnC5 = overlay.findViewById(getResources().getIdentifier("btnC5", "id", getPackageName()));
         btnC5.setOnClickListener(v -> NativeBridge.runMacro(NativeBridge.C_5));
 
         // Buttons that visibility are controlled by the toggle
@@ -286,12 +305,11 @@ public class GameActivity extends org.libsdl.app.SDLActivity {
             boolean topBarVisible = topBar[0].getVisibility() == View.VISIBLE;
             boolean joystickVisible = joystick[0].getVisibility() == View.VISIBLE;
 
-            for (View w : topBar) w.setVisibility(
-                    topBarVisible && joystickVisible ? View.GONE : View.VISIBLE
-            );
-            for (View w : joystick) w.setVisibility(
-                    topBarVisible && !joystickVisible ? View.VISIBLE : View.GONE
-            );
+            int newTop = (topBarVisible && joystickVisible) ? View.GONE : View.VISIBLE;
+            int newJoy = (topBarVisible && !joystickVisible) ? View.VISIBLE : View.GONE;
+
+            for (int i = 0; i < topBar.length; i++) topBar[i].setVisibility(newTop);
+            for (int i = 0; i < joystick.length; i++) joystick[i].setVisibility(newJoy);
         });
 
         for (View w : topBar) w.setVisibility(
@@ -308,6 +326,7 @@ public class GameActivity extends org.libsdl.app.SDLActivity {
         overlay.bringToFront();
         overlay.setElevation(10000f);
 
+
         if (Build.VERSION.SDK_INT >= 30) {
             final WindowInsetsController c = getWindow().getInsetsController();
             if (c != null) {
@@ -315,23 +334,5 @@ public class GameActivity extends org.libsdl.app.SDLActivity {
                 c.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
             }
         }
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    private View.OnTouchListener makeTouchHandler(int code) {
-        return (v, e) -> {
-            switch (e.getActionMasked()) {
-                case MotionEvent.ACTION_DOWN:
-                    v.setPressed(true);
-                    NativeBridge.onButton(code, true);
-                    return true;
-                case MotionEvent.ACTION_UP:
-                case MotionEvent.ACTION_CANCEL:
-                    v.setPressed(false);
-                    NativeBridge.onButton(code, false);
-                    return true;
-            }
-            return false;
-        };
     }
 }

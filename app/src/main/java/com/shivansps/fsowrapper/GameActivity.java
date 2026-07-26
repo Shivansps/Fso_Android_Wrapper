a4ed595d00ac447f3d8dd4f31c85afb18a4aea79
Salvador Cipolla <shivanuo@hotmail.com>
2026-07-26 11:15:03 -0300

    applyImmersive

--- app/src/main/java/com/shivansps/fsowrapper/GameActivity.java
+++ app/src/main/java/com/shivansps/fsowrapper/GameActivity.java
@@ -11,6 +11,9 @@ import android.view.*;
 import android.widget.*;
 import com.shivansps.fsowrapper.overlay.NativeBridge;
 import java.lang.ref.WeakReference;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 public class GameActivity extends org.libsdl.app.SDLActivity {
 
@@ -94,7 +97,7 @@ public class GameActivity extends org.libsdl.app.SDLActivity {
         try {
             getWindow().setSustainedPerformanceMode(true);
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-            hideSystemUI();
+            applyImmersive();
         } catch (Throwable ignored) {
         }
         TTSManager.init(this);
@@ -114,28 +117,26 @@ public class GameActivity extends org.libsdl.app.SDLActivity {
         super.onPause();
     }
 
-    private void hideSystemUI() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    private void applyImmersive() {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        WindowInsetsControllerCompat c =
+                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        c.hide(WindowInsetsCompat.Type.systemBars());
+        c.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            hideSystemUI();
+            applyImmersive();
         }
     }
 
     @Override protected void onResume() {
         super.onResume();
-        hideSystemUI();
+        applyImmersive();
     }
 
     @Override protected void onDestroy()

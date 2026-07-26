6578f0d9bdebd6655e2544ae46c65083b2c69494
Salvador Cipolla <shivanuo@hotmail.com>
2026-07-26 13:55:57 -0300

    enable no large shaders by default

--- app/src/main/java/com/shivansps/fsowrapper/MainActivity.java
+++ app/src/main/java/com/shivansps/fsowrapper/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String shader_file_name = "0_shaders_v5.vp";
     private static final String demo_filename = "fs2_demo.vpc"; //empty to disable demo install function
     private static final String LOG_RELATIVE_PATH = "data/fs2_open.log";
-    private static final String defaultArgs = "-fps -threads 0";
+    private static final String defaultArgs = "-fps -threads 0 -no_large_shaders";
     private Spinner spEngine;
     private Spinner spWorkingFolder;
     private RadioGroup rgRootSubdir;
